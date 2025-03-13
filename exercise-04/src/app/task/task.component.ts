42d1434a778c3860e325568b5fcbb7147ba70933
Tautminas Cibulskis <tautminas@yahoo.com>
2025-03-14 01:00:00 +0200

    Create localized date pipe and update styles

--- exercise-04/src/app/task/task.component.ts
+++ exercise-04/src/app/task/task.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { DatePipe } from '@angular/common';
+import { LocalizedDatePipe } from '../localized-date.pipe';
 
 @Component({
   selector: 'app-task',
-  imports: [DatePipe],
+  imports: [DatePipe, LocalizedDatePipe],
   templateUrl: './task.component.html',
   styleUrl: './task.component.scss',
 })
@@ -20,6 +21,22 @@ export class TaskComponent {
       'Learn Angular',
       'Write a blog post',
       'Clean the house',
+      'Prepare dinner',
+      'Go for a jog',
+      'Organize the closet',
+      'Meditate for 10 minutes',
+      'Update LinkedIn profile',
+      'Plan a trip',
+      'Work on a personal project',
+      'Attend a webinar',
+      'Watch a documentary',
+      'Practice coding challenges',
+      'Clean the kitchen',
+      'Call a family member',
+      'Plan the week ahead',
+      'Complete a puzzle',
+      'Watch a TED talk',
+      'Attend a fitness class',
     ];
 
     const newTask = {
