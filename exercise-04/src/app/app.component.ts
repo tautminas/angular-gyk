ff57cca562e337687160bf8dfdac91545e35582e
Tautminas Cibulskis <tautminas@yahoo.com>
2025-03-25 23:01:53 +0200

    Add home and tasks routes

--- exercise-04/src/app/app.component.ts
+++ exercise-04/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { TaskComponent } from './task/task.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [TaskComponent],
+  imports: [RouterOutlet],
 })
 export class AppComponent {}
--- exercise-04/src/app/app.routes.ts
+++ exercise-04/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { TaskComponent } from './task/task.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: HomeComponent,
+  },
+  {
+    path: 'tasks',
+    component: TaskComponent,
+  },
+  // { path: 'tasks/:id', component: TaskpageComponent },
+];
